a9350299db81136d2e632c7f9ee7be2a52e09869
Anton Liubimov <ant.liubimov@gmail.com>
2024-01-06 15:41:45 +0500

    refactor: Add greeting

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,12 @@
 import prompt
 
 
+def greeting():
+    print("Welcome to the Brain Games!")
+
+
 def welcome_user():
+    greeting()
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
     return name
