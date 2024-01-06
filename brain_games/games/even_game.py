d65f68b57658034431cd357d7f4bcb047e60af25
Anton Liubimov <ant.liubimov@gmail.com>
2024-01-06 15:46:00 +0500

    refactor: Change even-game

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,24 @@
+from random import randrange
+from brain_games.games.game import play_game
+
+EVEN_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even_number(number):
+    return number % 2 == 0
+
+
+def check_even(number):
+    return "yes" if is_even_number(number) else "no"
+
+
+def even_game_fn():
+  MIN_NUMBER = 0
+  MAX_NUMBER = 100
+  question = randrange(MIN_NUMBER, MAX_NUMBER)
+  answer = check_even(question)
+  return (question, answer)
+
+
+def even_game():
+  play_game(EVEN_RULES, even_game_fn)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,23 +1,5 @@
-import prompt
-from random import randrange
-from brain_games.cli import welcome_user
+from brain_games.games.even_game import even_game
 
 
 def main():
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    CORRECT_ANSWERS_COUNT = 3
-    answer_count = 0
-    while answer_count < CORRECT_ANSWERS_COUNT:
-        number = randrange(100)
-        answer = "yes" if number % 2 == 0 else "no"
-        print(f"Question: {number}")
-        your_answer = prompt.string("Your answer: ")
-        if answer != your_answer:
-            print(f"'{your_answer}' is wrong answer ;(. Correct answer was '{answer}'.")
-            print(f"Let's try again, {name}")
-            return
-        answer_count += 1
-        print("Correct!")
-
-    print(f"Congratulations, {name}")
+    even_game()
